0633b7ff027c57fd85bbb93b031cebb0649f72a5
Phut <25856150+phut-tran@users.noreply.github.com>
2022-02-24 21:27:58 +0700

    fix eslint

--- src/common/add-doc-button.js
+++ src/common/add-doc-button.js
@@ -1,6 +1,6 @@
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import AddIcon from '@mui/icons-material/Add';
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import AddIcon from '@mui/icons-material/Add'
 import { Link as RouterLink } from 'react-router-dom'
 import Link from '@mui/material/Link'
 
@@ -9,7 +9,7 @@ function AddDocButton() {
     <Link
       underline='none'
       sx={{ display: 'block', maxWidth: 275, border: '1px dashed #adadad' }}
-      data-testid="link-card"
+      data-testid='link-card'
       component={RouterLink}
       to={'/reading/new'}>
       <Box>
--- src/routes/reading-list.js
+++ src/routes/reading-list.js
@@ -1,7 +1,10 @@
+import AddDocButton from '../common/add-doc-button'
+
 const ReadingList = () => {
   return (
     <div>
       <h1>Reading List</h1>
+      <AddDocButton />
     </div>
   )
 }
